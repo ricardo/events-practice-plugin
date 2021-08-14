62bddb6907a632a3d420cb50fb232b96525f5d04
Jorge Calle <jorge.calle@automattic.com>
2021-08-14 12:23:37 -0500

    Add admin subpages

--- events-practice.php
+++ events-practice.php
@@ -20,13 +20,40 @@
  function eventspractice_settings_page()
  {
      add_menu_page(
-         'Events Practice',
-         'Events Practice',
+         __( 'Events Practice', 'eventspractice' ),
+         __( 'Events Practice', 'eventspractice' ),
          'manage_options',
          'eventspractice',
          'eventspractice_settings_page_markup',
          'dashicons-calendar-alt',
-         100
+         25
+     );
+
+     add_submenu_page(
+        'eventspractice',
+        __( 'Events', 'eventspractice' ),
+        __( 'Events', 'eventspractice' ),
+        'manage_options',
+        'wpplugin-events',
+        'eventspractice_settings_subpage_markup'
+     );
+
+     add_submenu_page(
+        'eventspractice',
+        __( 'RSVPs', 'eventspractice' ),
+        __( 'RSVPs', 'eventspractice' ),
+        'manage_options',
+        'wpplugin-rsvps',
+        'eventspractice_settings_subpage_markup'
+     );
+
+     add_submenu_page(
+        'eventspractice',
+        __( 'Settings', 'eventspractice' ),
+        __( 'Settings', 'eventspractice' ),
+        'manage_options',
+        'wpplugin-settings',
+        'eventspractice_settings_subpage_markup'
      );
  }
 add_action( 'admin_menu', 'eventspractice_settings_page' );
@@ -42,6 +69,21 @@ function eventspractice_settings_page_markup()
     <div class="wrap">
         <h1><?php esc_html_e( get_admin_page_title(), 'eventspractice' ); ?></h1>
         <p><?php esc_html_e( 'Some content.', 'eventspractice' ); ?></p>
+    </div>
+    <?php
+}
+
+// Display content for the admin subpages
+function eventspractice_settings_subpage_markup()
+{
+    // Double check user capabilities
+    if ( !current_user_can('manage_options') ) {
+        return;
+    }
+    ?>
+    <div class="wrap">
+        <h1><?php esc_html_e( get_admin_page_title(), 'eventspractice' ); ?></h1>
+        <p><?php esc_html_e( 'Some subpage content.', 'eventspractice' ); ?></p>
     </div>
     <?php
 }
