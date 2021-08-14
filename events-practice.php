828ad50d616726c3ad9cd9b4c0f6515a8af17f04
Jorge Calle <jorge.calle@automattic.com>
2021-08-14 12:10:07 -0500

    Add main admin page

--- events-practice.php
+++ events-practice.php
@@ -7,7 +7,7 @@
  * Author:            Jorge Calle
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       events-practice
+ * Text Domain:       eventspractice
  * Domain Path:       /languages
  */
 
@@ -15,3 +15,35 @@
  if ( ! defined( 'WPINC' ) ) {
      die;
  }
+
+ // Add page to WP Admin
+ function eventspractice_settings_page()
+ {
+     add_menu_page(
+         'Events Practice',
+         'Events Practice',
+         'manage_options',
+         'eventspractice',
+         'eventspractice_settings_page_markup',
+         'dashicons-calendar-alt',
+         100
+     );
+ }
+add_action( 'admin_menu', 'eventspractice_settings_page' );
+
+// Display content for the admin page
+function eventspractice_settings_page_markup()
+{
+    // Double check user capabilities
+    if ( !current_user_can('manage_options') ) {
+        return;
+    }
+    ?>
+    <div class="wrap">
+        <h1><?php esc_html_e( get_admin_page_title(), 'eventspractice' ); ?></h1>
+        <p><?php esc_html_e( 'Some content.', 'eventspractice' ); ?></p>
+    </div>
+    <?php
+}
+
+?>
